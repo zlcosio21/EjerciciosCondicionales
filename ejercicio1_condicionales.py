af23bd880223b91fe928ce1e097526b1b6d981ca
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-08-09 00:00:19 -0500

    Ejercicio 1 actualizado

--- ejercicio1_condicionales.py
+++ ejercicio1_condicionales.py
@@ -1,18 +1,14 @@
 #En una fábrica de computadoras se planea ofrecer a los clientes un descuento que dependerá del número de computadoras que compren. Si las computadoras son menos de cinco se les dará un 10% de descuento sobre el total de la compra; si el número de computadoras es mayor o igual a cinco pero menos de diez se les otorga un 20% de descuento; y si son diez o más se les da un 40%. El precio de cada computadora es de $1.000.000
 
+num_computadoras = int(input("Ingrese el numero de computadoras a comprar: "))
 
-computer_number = int(input("enter the number of cumputers to buy: "))
+if num_computadoras < 5:
+  descuento = 0.90
+elif num_computadoras >= 5 and num_computadoras < 10:
+  descuento = 0.80
+elif num_computadoras  >= 10:
+  descuento = 0.60
 
+total = num_computadoras * 1000000 * descuento
 
-if computer_number < 5:
-  discount = 0.1
-elif computer_number >= 5 and computer_number < 10:
-  discount = 0.2
-if computer_number  >= 10:
-  discount = 0.4
-
-
-total = computer_number * 1000000 - (computer_number * 1000000 * discount)
-
-
-print(f"Total to pay: ${total} COP")
+print(f"Total to pay: ${total:,} COP")
